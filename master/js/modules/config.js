b0be77fba56f6d16b803bf060f8f685857680e09
Francisco X. Alvarez <franciscoalvarezd@gmail.com>
2015-09-28 13:00:09 -0700

    going back to hash bang

--- master/js/modules/config.js
+++ master/js/modules/config.js
@@ -9,11 +9,11 @@ function ($stateProvider, $locationProvider, $urlRouterProvider, helper) {
 
     // Set the following to true to enable the HTML5 Mode
     // You may have to set <base> tag in index and a routing configuration in your server
-    $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(false);
     $locationProvider.hashPrefix('!');
 
     // default route
-    // $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
     // -----------------------------------   
     // Application Routes
@@ -41,7 +41,7 @@ function ($stateProvider, $locationProvider, $urlRouterProvider, helper) {
                 templateUrl: helper.basepath('services.html')
         })
         .state('app.services.video', {
-                url: '/services',
+                url: '/services-intro',
                 title: 'Services',
                 templateUrl: helper.basepath('services/services-video.html')
         })
